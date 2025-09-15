5252920dd912f8ddc953b2e0ce6d4fda9556a79e
JMQ0481 <jaigonsor@alum.us.es>
2025-09-15 20:25:30 +0200

    Dar de alta a JMQ0481 como jugador

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,3 +16,4 @@ INSERT INTO appusers(id,username,password,authority) VALUES (12,'player9','$2a$1
 INSERT INTO appusers(id,username,password,authority) VALUES (13,'player10','$2a$10$DaS6KIEfF5CRTFrxIoGc7emY3BpZZ0.fVjwA3NiJ.BjpGNmocaS3e',2);
 
 INSERT INTO appusers(id,username,password,authority) VALUES (14,'jescarcon','$2a$10$DaS6KIEfF5CRTFrxIoGc7emY3BpZZ0.fVjwA3NiJ.BjpGNmocaS3e',2);
+INSERT INTO appusers(id,username,password,authority) VALUES (15,'JMQ0481','$2a$10$DaS6KIEfF5CRTFrxIoGc7emY3BpZZ0.fVjwA3NiJ.BjpGNmocaS3e',2);

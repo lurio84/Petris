5dec34c6bf24e80a4937b7098c491f63d82e02dd
jescarcon <jesuscardenas2000@gmail.com>
2025-10-25 14:41:34 +0200

    Java class Authorities constraints according to UML

--- src/main/java/es/us/dp1/l4_01_25_26/petris/user/Authorities.java
+++ src/main/java/es/us/dp1/l4_01_25_26/petris/user/Authorities.java
@@ -4,6 +4,8 @@ import es.us.dp1.l4_01_25_26.petris.model.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,8 +20,9 @@ public class Authorities extends BaseEntity{
 //	User user;
 	
 //	@Enumerated(EnumType.STRING)
-	@Column(length = 20)
-	String authority;
-	
+	@Column(length = 30, unique = true) 
+	@NotBlank(message = "Authority can't be blank") 
+	@Size(max = 30, message = "Authority must have at most 30 characters") 
+	private String authority; 
 	
 }

43db5d77ac85a00df7a271889559d3b40f9adb0c
jescarcon <jesuscardenas2000@gmail.com>
2025-11-02 14:03:46 +0100

    Metric & Sql updated

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -25,22 +25,22 @@ INSERT INTO appusers(id,username,password,authority) VALUES
 (18,'HGK2646','$2a$10$DaS6KIEfF5CRTFrxIoGc7emY3BpZZ0.fVjwA3NiJ.BjpGNmocaS3e',2),
 (19,'XDW2012','$2a$10$DaS6KIEfF5CRTFrxIoGc7emY3BpZZ0.fVjwA3NiJ.BjpGNmocaS3e',2);
 
-INSERT INTO achievement (id,name,description,badge_image,threshold,metric) VALUES
- (1,'Principiante','Juega tu primera partida','https://doctor-cv.com/wp-content/uploads/2020/08/gold-trophy-with-name-plate-winner-competition_68708-545-1.jpg',1,'GAMES_PLAYED'),
- (2,'Aprobado','Gana tu primera partida','https://doctor-cv.com/wp-content/uploads/2020/08/gold-trophy-with-name-plate-winner-competition_68708-545-1.jpg',1,'VICTORIES'),
- (3,'Graduado','Gana 10 partidas','https://doctor-cv.com/wp-content/uploads/2020/08/gold-trophy-with-name-plate-winner-competition_68708-545-1.jpg',10,'VICTORIES'),
- (4,'Experimentado','Juega 20 partidas','https://doctor-cv.com/wp-content/uploads/2020/08/gold-trophy-with-name-plate-winner-competition_68708-545-1.jpg',20,'GAMES_PLAYED'),
- (5,'Doctor','Gana 40 partidas','https://doctor-cv.com/wp-content/uploads/2020/08/gold-trophy-with-name-plate-winner-competition_68708-545-1.jpg',10,'VICTORIES'),
- (6,'Verde Novato','Juega 5 partidas como equipo verde','https://doctor-cv.com/wp-content/uploads/2020/08/gold-trophy-with-name-plate-winner-competition_68708-545-1.jpg',5,'GAMES_AS_GREEN'),
- (7,'Púrpura Novato','Juega 5 partidas como equipo púrpura','https://doctor-cv.com/wp-content/uploads/2020/08/gold-trophy-with-name-plate-winner-competition_68708-545-1.jpg',5,'GAMES_AS_PURPLE'),
- (8,'Verde Experto','Juega 25 partidas como equipo verde','https://doctor-cv.com/wp-content/uploads/2020/08/gold-trophy-with-name-plate-winner-competition_68708-545-1.jpg',25,'GAMES_AS_GREEN'),
- (9,'Púrpura Experto','Juega 25 partidas como equipo púrpura','https://doctor-cv.com/wp-content/uploads/2020/08/gold-trophy-with-name-plate-winner-competition_68708-545-1.jpg',25,'GAMES_AS_PURPLE'),
- (10,'Victoria Verde','Gana tu primera partida como equipo verde','https://doctor-cv.com/wp-content/uploads/2020/08/gold-trophy-with-name-plate-winner-competition_68708-545-1.jpg',1,'VICTORIES_AS_GREEN'),
- (11,'Victoria Púrpura','Gana tu primera partida como equipo púrpura','https://doctor-cv.com/wp-content/uploads/2020/08/gold-trophy-with-name-plate-winner-competition_68708-545-1.jpg',1,'VICTORIES_AS_PURPLE'),
- (12,'Bacteria Pro','Coloca 5 bacterias en una partida','https://doctor-cv.com/wp-content/uploads/2020/08/gold-trophy-with-name-plate-winner-competition_68708-545-1.jpg',5,'MAX_BACTERY_PLAYED'),
- (13,'Sarcina Pro','Coloca 3 sarcinas en una partida','https://doctor-cv.com/wp-content/uploads/2020/08/gold-trophy-with-name-plate-winner-competition_68708-545-1.jpg',3,'MAX_SARCINE_PLAYED'),
- (14,'Resistencia','Completa una partida de 30 turnos','https://doctor-cv.com/wp-content/uploads/2020/08/gold-trophy-with-name-plate-winner-competition_68708-545-1.jpg',30,'MAX_TURNS_PLAYED')
- (15,'Maratonista','Completa una partida de 60 turnos','https://doctor-cv.com/wp-content/uploads/2020/08/gold-trophy-with-name-plate-winner-competition_68708-545-1.jpg',60,'MAX_TURNS_PLAYED');
+INSERT INTO achievements (id,name,description,badge_image,threshold,metric) VALUES
+ (1,'Principiante','Juega tu primera partida','imagen1',1,'GAMES_PLAYED'),
+ (2,'Aprobado','Gana tu primera partida','imagen1',1,'GAMES_WON'),
+ (3,'Graduado','Gana 10 partidas','imagen1',10,'GAMES_WON'),
+ (4,'Experimentado','Juega 20 partidas','imagen1',20,'GAMES_PLAYED'),
+ (5,'Doctor','Gana 40 partidas','imagen1',10,'GAMES_WON'),
+ (6,'Verde Novato','Juega 5 partidas como equipo verde','imagen1',5,'GAMES_AS_GREEN'),
+ (7,'Púrpura Novato','Juega 5 partidas como equipo púrpura','imagen1',5,'GAMES_AS_PURPLE'),
+ (8,'Verde Experto','Juega 25 partidas como equipo verde','imagen1',25,'GAMES_AS_GREEN'),
+ (9,'Púrpura Experto','Juega 25 partidas como equipo púrpura','imagen1',25,'GAMES_AS_PURPLE'),
+ (10,'Victoria Verde','Gana tu primera partida como equipo verde','imagen1',1,'VICTORIES_AS_GREEN'),
+ (11,'Victoria Púrpura','Gana tu primera partida como equipo púrpura','imagen1',1,'VICTORIES_AS_PURPLE'),
+ (12,'Bacteria Pro','Coloca 5 bacterias en una partida','imagen1',5,'MAX_BACTERY_PLAYED'),
+ (13,'Sarcina Pro','Coloca 3 sarcinas en una partida','imagen1',3,'MAX_SARCINE_PLAYED'),
+ (14,'Resistencia','Completa una partida de 30 turnos','imagen1',30,'MAX_TURNS_PLAYED'),
+ (15,'Maratonista','Completa una partida de 60 turnos','imagen1',60,'MAX_TURNS_PLAYED');
 
 -- Estadísticas de jugadores base
 INSERT INTO player_statistics(

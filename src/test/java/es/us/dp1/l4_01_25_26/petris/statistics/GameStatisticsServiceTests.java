4195cf056c90e555a409eab828a7d5790b7feba1
lurio84 <lucas.ronquillo@gmail.com>
2025-11-17 12:12:02 +0100

    GameStatisticsServiceTests.java terminados, falta comprobar que funcionan

--- src/test/java/es/us/dp1/l4_01_25_26/petris/statistics/GameStatisticsServiceTests.java
+++ src/test/java/es/us/dp1/l4_01_25_26/petris/statistics/GameStatisticsServiceTests.java
@@ -11,8 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import es.us.dp1.l4_01_25_26.petris.statistic.GameStatistics;
-import es.us.dp1.l4_01_25_26.petris.statistic.GameStatisticsService;
+import es.us.dp1.l4_01_25_26.petris.game.GameStatistics.GameStatistics;
+import es.us.dp1.l4_01_25_26.petris.game.GameStatistics.GameStatisticsService;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
@@ -20,8 +20,8 @@ import io.qameta.allure.Owner;
 @Epic("Statistics Module")
 @Feature("Game Statistics")
 @Owner("DP1_L4-1_Developers")
-@SpringBootTest
-@AutoConfigureTestDatabase
+//@SpringBootTest
+//@AutoConfigureTestDatabase
 public class GameStatisticsServiceTests {
 
     @Autowired
@@ -87,4 +87,25 @@ public class GameStatisticsServiceTests {
 
     // ###################################### DELETE ######################################
 
+    @Test //204
+    void shouldDeleteGameStatistics() {
+        int initialSize = gameStatisticsService.findAll().size();
+
+        GameStatistics newStats = new GameStatistics();
+        newStats.setTurnsPlayed(15);
+        newStats.setTotalBacteries(60);
+        newStats.setTotalSarcines(40);
+        newStats.setWinner("purple");
+        newStats.setGreenPlayerBacteries(25);
+        newStats.setGreenPlayerSarcines(15);
+        newStats.setPurplePlayerBacteries(35);
+        newStats.setPurplePlayerSarcines(25);
+
+        GameStatistics savedStats = gameStatisticsService.save(newStats);
+        assertThat(gameStatisticsService.findAll().size()).isEqualTo(initialSize+1);
+
+        gameStatisticsService.delete(savedStats.getId());
+        assertThat(gameStatisticsService.findAll().size()).isEqualTo(initialSize);
+        assertThat(gameStatisticsService.findById(savedStats.getId()));
+    }
 }

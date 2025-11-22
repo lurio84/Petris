cc42f5d1b8ba25e5a87a1b96ff7f0f7266d4ede7
Antonio José López Cubiles <antlopcub@alum.us.es>
2025-11-22 13:06:36 +0100

    Version preliminar de Respositorio y DTO de PetriPlate

--- src/main/java/es/us/dp1/l4_01_25_26/petris/game/utils/PetriPlate.java
+++ src/main/java/es/us/dp1/l4_01_25_26/petris/game/utils/PetriPlate.java
@@ -2,6 +2,9 @@ package es.us.dp1.l4_01_25_26.petris.game.utils;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,5 +25,12 @@ public class PetriPlate {
     @NotNull(message = "The list of purple sarcines must not be null")
     private List<Microorganism> purpleSarcines = new ArrayList<>();
 
+    @NotNull(message = "The position of the tile must not be null")
+    @Min(1)
+    @Max(7)
+    private Integer position;
+
+    @NotNull
+    private List<Integer> adjacentTiles;
 
 }
---
+++ src/main/java/es/us/dp1/l4_01_25_26/petris/game/utils/PetriPlateRepository.java
@@ -0,0 +1,17 @@
+package es.us.dp1.l4_01_25_26.petris.game.utils;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetriPlateRepository extends CrudRepository<PetriPlate, Integer> {
+    List<PetriPlate> findAll();
+    Optional<PetriPlate> findById(Integer id);
+
+    @Query("SELECT pt FROM PetriPlate pt WHERE pt.position = :position")
+    List<PetriPlate> findByPosition(Integer position);
+
+    //HAbría que añadir un Delet by Game id si tuvieramos persistencia en BBDD
+}
